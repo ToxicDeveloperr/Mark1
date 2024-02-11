9404b703512be21ee39d7fb9d19a559a24d4209e
ToxicDeveloperr <154732531+ToxicDeveloperr@users.noreply.github.com>
2024-02-11 08:22:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tejaschavan1110:v6C6ZNSty2AmoMOs@cluster0.20nyiir.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tejaschavan1110:LepNK2GgdGDd7gB9@cluster0.r66lbro.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
